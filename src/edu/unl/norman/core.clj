874c43236add41644f912f6ab27a37fba295a463
Stephen Ramsay <sramsay.unl@gmail.com>
2012-06-11 12:21:50 -0500

    Some basic refactoring.

--- src/edu/unl/norman/core.clj
+++ src/edu/unl/norman/core.clj
@@ -7,7 +7,7 @@
 ;;; Written and maintained by Stephen Ramsay for the Center for
 ;;; Digital Research in the Humanities at the University of Nebraska-Lincoln.
 ;;;
-;;; Last Modified: Sun May 27 13:13:02 CDT 2012
+;;; Last Modified: Mon Jun 11 11:08:26 CDT 2012
 ;;;
 ;;; Copyright © 2011-2012 Board of Regents of the University of Nebraska-
 ;;; Lincoln (and others).  See LICENSE for details.
@@ -26,7 +26,7 @@
 (require '[clojure.tools.cli :as c]
 				 '[saxon :as sax])
 
-(def version "0.0.1")
+(def version "0.0.2")
 
 (def norman-home (System/getenv "NORMAN_HOME"))
 
@@ -46,27 +46,27 @@
   ; in the specification, but nowhere mandated.
   ;
   ; So we check that the file is, in fact, a file, and demand an .xml
-  ; extension.  Notification of more insidious file errors will have
-  ; to be left to Saxon.
+  ; extension.  Notification of more insidious file errors is left to
+  ; Saxon itself.
     (let [filters [#(.isFile %)
                                  #(has-xml-extension? %)]]
     (filter (fn [x] (every? #(% x) filters)) (file-seq (File. input-dir)))))
 
-(defn convert [output-dir stylesheet]
+(defn converter [output-dir stylesheet]
     "Returns a function that runs the conversion and writes out the file"
   (let [convert (fn [stylesheet xml-file]
-                  (let [xmlfile (sax-compile-xml xml-file)]
+                  (let [xmlfile (sax/compile-xml xml-file)]
                     (stylesheet xmlfile)))]
     ; Written as a clojure to keep the main convert-files function
     ; uncluttered.  
-    (fn [x] (spit (str output-dir (.getName x)) (convert stylesheet x))))
+    (fn [x] (spit (str output-dir (.getName x)) (convert stylesheet x)))))
 
 (defn convert-files [{input-dir  :inputdir
                       output-dir :outputdir
                       stylesheet :stylesheet}]
   "Apply the conversion stylesheet to the input files."
     (let [stylesheet (sax/compile-xslt (slurp stylesheet))
-          converter (convert output-dir stylesheet)]
+          converter (converter output-dir stylesheet)]
         (doall (pmap converter (input-files input-dir)))))
 
 (defn -main [& args]
