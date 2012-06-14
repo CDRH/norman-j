c988697eeefc9e7c87907c27ae406d6db134bc03
Stephen Ramsay <sramsay.unl@gmail.com>
2012-06-14 14:43:44 -0500

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edu.unl/norman "0.0.4"
+(defproject edu.unl/norman "0.0.5"
   :description "Norman: A Parallelizing XSLT Processor based on Saxon"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
