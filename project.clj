f0dbf5eee3d1493d01162c848132f278b3c07f1b
Stephen Ramsay <sramsay.unl@gmail.com>
2013-01-14 10:12:54 -0600

    Updated dependencies for tools-cli and clojure (to RC2).

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject edu.unl/norman "0.1.1"
+(defproject edu.unl/norman "0.1.2"
   :description "Norman: A Parallelizing XSLT Processor based on Saxon"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.cli "0.2.1"]
-                 [clojure-saxon "0.9.2"]]
+  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
+                 [org.clojure/tools.cli "0.2.2"]
+                 [clojure-saxon "0.9.3"]]
 								 :main edu.unl.norman.core)
 
