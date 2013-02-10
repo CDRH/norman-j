aa37d62849bb31499bcba0d1401438ae833bfe71
Stephen Ramsay <sramsay.unl@gmail.com>
2013-02-10 15:05:45 -0600

    RC14

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject edu.unl/norman "0.1.2"
   :description "Norman: A Parallelizing XSLT Processor based on Saxon"
-  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC14"]
                  [org.clojure/tools.cli "0.2.2"]
                  [clojure-saxon "0.9.3"]]
 								 :main edu.unl.norman.core)
