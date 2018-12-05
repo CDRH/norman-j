fff32ca6cf6a77c29d7f8d9e178fff10c6773365
Stephen Ramsay <sramsay@protonmail.com>
2018-12-05 16:53:52 -0600

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject edu.unl/norman "0.2.0"
+(defproject edu.unl/norman "1.0.0"
   :description "Norman: A Parallelizing XSLT Processor based on Saxon"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.2"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [clojure-saxon "0.9.3"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [clojure-saxon "0.9.4"]
                  [log4j "1.2.17"]]
   :main edu.unl.norman.core
+  :aot [edu.unl.norman.core]
   :jar-name "norman.jar"
-  :uberjar-name "norman-0.2.0.jar")
+  :uberjar-name "norman-1.0.0.jar")
